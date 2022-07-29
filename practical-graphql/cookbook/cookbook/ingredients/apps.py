4c7cd80ccae4f9178594db6799a1aa7c09256622
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-07-29 11:46:57 -0300

    added graphene

--- practical-graphql/cookbook/cookbook/ingredients/apps.py
+++ practical-graphql/cookbook/cookbook/ingredients/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class IngredientsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ingredients'
+    name = 'cookbook.ingredients'
--- practical-graphql/cookbook/cookbook/settings.py
+++ practical-graphql/cookbook/cookbook/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
+    'cookbook.ingredients',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+GRAPHENE = {
+    "SCHEMA": "django_root.schema.schema"
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
